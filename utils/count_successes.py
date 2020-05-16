eb74bdf4baf6fb3c8d8649e04d3c7bd69e995e26
DmitriiDenisov <dmitryhse@gmail.com>
2020-05-16 13:18:51 +0400

    Refactoring

--- utils/count_successes.py
+++ utils/count_successes.py
@@ -7,9 +7,13 @@ import pandas as pd
 from utils.constants import DATASETS, MODES
 from utils.utils_py import get_number_success
 
-df_result = pd.read_excel('3.xlsx', index_col=None)
+df_result = pd.read_excel('5.xlsx', index_col=None)
 df_result = df_result.dropna(how='all').reset_index(drop=True)
 
-for mode, step in [['smote', 4], ['ADASYN', 8], ['OVERSAMP', 12], ['UNDERSAMP', 16], ['smote+normal', 20]]:
+all_success = 0
+list_comp = [['smote', 4], ['ADASYN', 8], ['OVERSAMP', 12], ['UNDERSAMP', 16], ['smote+normal', 20]]
+for mode, step in list_comp:
     success = get_number_success(df_result, index=4, step=step, num_modes=7)
+    all_success += success
     print(f'Gamma VS {mode}. Success: {success} out of {len(DATASETS) * 4}')
+print(f'Total number of success:{all_success} out of {len(list_comp) * len(DATASETS) * 4}')
